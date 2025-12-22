bbae0bc7ba0b880ab44a252c218f195ccd8e58d4
Nattawut.c <blackpuppy.dev@gmail.com>
2025-12-22 12:12:27 +0700

    commit 22/12/2025 - fix main.py

--- api/main.py
+++ api/main.py
@@ -1,4 +1,11 @@
+import sys
+import os
 from fastapi import FastAPI, HTTPException, UploadFile, File, Form, Depends, Query 
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
 from core.predictor import load_model_system, predict_inventory_usage, get_reference_data 
 from starlette.middleware.cors import CORSMiddleware
 import logging
